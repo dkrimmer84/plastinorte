c8d0848ea6cd99f4ef859a555c5f413069ef543d
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2019-11-13 08:39:42 -0400

    Renaming file to manifest

---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+
+{
+    'name': 'Plastinorte',
+    'category': 'Localization',
+    'version': '12.0.0.1.0',
+    'author': 'Dominic Krimmer, Plastinorte S.A.S',
+    'license': 'AGPL-3',
+    'maintainer': 'dominic.krimmer@gmail.com',
+    'website': 'https://www.plastinorte.com',
+    'summary': 'Changes made for Plastinorte S.A.S ',
+    'depends': [
+        'point_of_sale',
+        'stock',
+        'account',
+        'hr_expense',
+        'hr',
+	'fleet',
+        'sale'
+    ],
+    'installable': True,
+    'data': [
+        'views/product.xml',
+        'wizard/pos_box.xml',
+        'views/category.xml',
+        'views/register_expense.xml',
+        'views/scripts.xml',
+	'views/fleet.xml',
+	'views/report_deliveryslip.xml',
+	'views/report_saleorder.xml',
+        'views/point_of_sale.xml',
+    ],
+    'qweb': [
+        "static/src/xml/inherit.xml",
+    ],
+}
