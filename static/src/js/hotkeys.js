c9431493d405f053c9dd851d1358bd580c343d98
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2019-11-13 09:42:41 -0400

    Removing unnecesary imports to hotkeys.js

--- static/src/js/hotkeys.js
+++ static/src/js/hotkeys.js
@@ -4,17 +4,8 @@ odoo.define('plastinorte', function (require) {
 	// Se importan los modulos necesarios
 	var core = require('web.core');
 	var models = require('point_of_sale.models');
-	var session = require('web.session');
-	var PosDB = require('point_of_sale.DB');
-	var gui = require('point_of_sale.gui');
-	var chrome = require('point_of_sale.chrome');
-	var ajax = require('web.ajax');
-	var Model = require('web.Model');
-	var data = require('web.data');
-	var PosBaseWidget = require('point_of_sale.BaseWidget');
 	var Screens = require('point_of_sale.screens');
 
-	var QWeb = core.qweb;
 	var _t = core._t;
 
 
